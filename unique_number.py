4e7b4b97442edc38c29aa6f26815f5b1cd78be6c
Willam Lubenow <wkl22@humboldt.edu>
2022-03-27 10:53:41 -0700

    Unique integer completed.

---
+++ unique_number.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    # Loop through the array.
+    # Only want the number that shows once, if the count of a number is 1 give us the number.
+    for num in a:  
+        if a.count(num) == 1:
+            return num
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
